44f41d2c8f511bbe884d5127a9459e35d0a63ece
Rafael Rodrigues <rafael-rafa2.1@hotmail.com>
2022-07-25 20:44:43 -0300

    Create project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/Button';
+
+export function App() {
+  return (
+    <div>
+      <Button variant='primary' />
+      <Button variant='secondary' />
+      <Button variant='danger' />
+      <Button variant='success' />
+      <Button />
+    </div>
+  );
+}
